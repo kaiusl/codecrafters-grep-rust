ff5736ebd86426e8870a5fc0a564f28d44d9a0d3
Kaius Loos <37976028+kaiusl@users.noreply.github.com>
2024-08-25 21:26:35 +0300

    match digits (stage 2)
    Also refactor a bit and also match combinations of single character and digits.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,6 @@ use std::env;
 use std::io;
 use std::process;
 
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
-    }
-}
-
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -25,10 +17,140 @@ fn main() {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
+    let regex = Regex::new(&pattern);
+
     // Uncomment this block to pass the first stage
-    if match_pattern(&input_line, &pattern) {
+    if regex.matches(&input_line) {
         process::exit(0)
     } else {
         process::exit(1)
+    }
+}
+
+#[derive(Debug)]
+struct Regex {
+    pattern: Vec<PatternElement>,
+}
+
+impl Regex {
+    pub fn new(pattern: &str) -> Self {
+        Self {
+            pattern: Self::parse_regex(pattern),
+        }
+    }
+
+    fn parse_regex(pattern: &str) -> Vec<PatternElement> {
+        let mut patterns = Vec::new();
+        let mut chars = pattern.chars();
+
+        while let Some(c) = chars.next() {
+            match c {
+                '\\' => {
+                    if let Some(c) = chars.next() {
+                        match c {
+                            'd' => patterns.push(PatternElement::Digit),
+                            _ => panic!("Invalid regex: {}", pattern),
+                        }
+                    } else {
+                        patterns.push(PatternElement::Literal(c));
+                    }
+                }
+                c => patterns.push(PatternElement::Literal(c)),
+            }
+        }
+        patterns
+    }
+
+    pub fn matches(&self, mut input: &str) -> bool {
+        let mut patterns = self.pattern.iter();
+
+        // First find the start of first pattern element.
+        // This can be anywhere in the input and is thus special case.
+        // All following pattern elements must directly follow the first one.
+        // Thus if we set the input to an input after the first match
+        // then the for loop below thus must match pattern at the start of input.
+
+        if let Some(p) = patterns.next() {
+            match p {
+                PatternElement::Literal(c) => {
+                    let Some(i) = input.find(*c) else {
+                        return false;
+                    };
+                    input = input.get(i + 1..).unwrap_or_default();
+                }
+                PatternElement::Digit => {
+                    let Some(i) = input.chars().position(|c| c.is_numeric()) else {
+                        return false;
+                    };
+                    input = input.get(i + 1..).unwrap_or_default();
+                }
+            }
+        }
+
+        for p in patterns {
+            match p {
+                PatternElement::Literal(c) => {
+                    if !input.starts_with(*c) {
+                        return false;
+                    }
+                    input = input.get(1..).unwrap_or_default();
+                }
+                PatternElement::Digit => {
+                    if let Some(c) = input.chars().next() {
+                        if !c.is_numeric() {
+                            return false;
+                        }
+                        input = input.get(1..).unwrap_or_default();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        true
+    }
+}
+
+#[derive(Debug)]
+enum PatternElement {
+    Literal(char),
+    Digit,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_single_char() {
+        let regex = Regex::new("a");
+        assert!(regex.matches("a"));
+        assert!(regex.matches("apple"));
+        assert!(!regex.matches("b"));
+        assert!(!regex.matches("bhsrt"));
+    }
+
+    #[test]
+    fn test_digit() {
+        let regex = Regex::new(r"\d");
+        dbg!(&regex);
+        assert!(regex.matches("1"));
+        assert!(regex.matches("apple2"));
+        assert!(!regex.matches("b"));
+        assert!(!regex.matches("bhsrt"));
+    }
+
+    #[test]
+    fn test_char_digit() {
+        let regex = Regex::new(r"a\d");
+        assert!(regex.matches("a1"));
+        assert!(!regex.matches("apple2"));
+        assert!(regex.matches("ba3"));
+        assert!(!regex.matches("bhsrt"));
+
+        let regex = Regex::new(r"\d apple\d\dyu");
+        assert!(regex.matches("5 apple16yu"));
+        assert!(!regex.matches("5 appe16yu"));
     }
 }
